b3bc9b26c95a8e49db0cc3cdb8660d750647024e
Asaf Fisher <asaffisher.dev@gmail.com>
2023-01-26 22:05:26 +0200

    Fix unhandled result and removed unused import

--- core/src/arch/hook.rs
+++ core/src/arch/hook.rs
@@ -1,6 +1,5 @@
 use alloc::vec;
 use alloc::{string::String, vec::Vec};
-use hal::Hal;
 
 use super::x86_64::branch::{generate_absolute_branch, ABSOLUTE_BRANCH_SIZE};
 use super::x86_64::patcher::Patcher;
@@ -194,7 +193,9 @@ where
 impl<T: Trampoline> Drop for DetourHook<T> {
     fn drop(&mut self) {
         unsafe {
-            self.disable();
+            // unwrap because drop does not return a result.
+            // If this operation fails we are dead...
+            self.disable().unwrap();
         }
     }
 }
